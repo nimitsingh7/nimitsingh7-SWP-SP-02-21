6fc6530acb779d11ea19c074142c2834e07ec216
nimitsingh7 <43422326+nimitsingh7@users.noreply.github.com>
2021-12-01 22:12:08 +0100

    Add files via upload

--- Schere_Stein_Papier_Echse_Spock.py
+++ Schere_Stein_Papier_Echse_Spock.py
@@ -12,12 +12,12 @@ while True:
 
     d = int(input("Deine Wahl(1-5): "))
     print("-> ", auswahl_liste[d])
-    print("Deine Punkte: " + str(spieler), "\n")
+    #print("Deine Punkte: " + str(spieler), "\n")
 
     print("Computer Wahl(1-5): ")
     e = random.randrange(1, 6)
     print("-> ", auswahl_liste[e])
-    print("Computer Punkte: ", str(computer), "\n")
+    #print("Computer Punkte: ", str(computer), "\n")
 
     # Schere
     if d == 1 and (e == 2 or e == 4):
@@ -31,33 +31,48 @@ while True:
     # Papier
     elif d == 2 and (e == 3 or e == 5):
         print(win)
-
+        spieler = spieler + 1
     elif (d == 3 or d == 5) and e == 2:
         print(loose)
 
     # Stein
     elif d == 3 and (e == 4 or e == 1):
         print(win)
-
+        spieler = spieler + 1
     elif (d == 4 or d == 1) and e == 3:
         print(loose)
+        computer = computer + 1
 
     # Echse
     elif d == 4 and (e == 5 or e == 2):
         print(win)
+        spieler = spieler + 1
     elif (d == 5 or d == 2) and e == 4:
         print(loose)
+        computer = computer + 1
 
     # Spock
     elif d == 5 and (e == 1 or e == 3):
         print(win)
+        spieler = spieler + 1
     elif (d == 1 or d == 3) and e == 5:
         print(loose)
+        computer = computer + 1
     else:
         print("----GLEICHSTAND!----")
-
     print("\n")
 
-    nochml = input('Nochmal? [ j / n]: ')
-    if nochml == "n":
+    nochmal = input("Wollen sie nochmal spielen? [j / n]: ")
+    if nochmal == "n":
         break
+
+# Daten in eine .txt Datei einlesen
+file = open("Textdatei.txt", "w")
+
+file.write("Ergbenisse vom Spieler: " + str(spieler))
+file.write("\n")
+file.write("Ergebnisse vom Computer: " + str(computer))
+file.write("\n")
+file.write("So oft wurde gespielt: ")
+
+file.close()
