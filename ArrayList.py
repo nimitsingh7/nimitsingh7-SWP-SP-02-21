d629b85b51f70ac95fba132353c2a6785ef3d8b8
nimitsingh7 <43422326+nimitsingh7@users.noreply.github.com>
2022-03-23 21:31:09 +0100

    Add files via upload

--- ArrayList.py
+++ ArrayList.py
@@ -7,18 +7,41 @@ class ArrayList:
     def __init__(self):
         self.list = []
 
-    def insertStart(self, neueDaten):
-        self.list.insert(0, neueDaten)
+    def addStart(self, newData):
+        self.list.insert(0, newData)
 
-    def insertEnd(self, neueDaten):
-        self.list.append(neueDaten)
+    def addEnd(self, newData):
+        self.list.append(newData)
+
+    def addAfter(self, pos, data):
+        self.list.insert(pos + 1, data)
+
+    def addBefore(self, pos, data):
+        if pos - 1 >= 0:
+            self.list.insert(pos - 1, data)
+
+    def delete(self, pos):
+        self.list.pop(pos)
+
+    def deleteAfter(self, pos):
+        if len(self.list) > pos + 1:
+            self.list.pop(pos + 1)
+
+    def deleteBefore(self, pos):
+        if pos - 1 >= 0:
+            self.list.pop(pos - 1)
 
     def length(self):
         return len(self.list)
 
-    def insertionSort(self):
-        for i in range(1, len(self.list)):
+    def findByIndex(self, index):
+        if self.list is None:
+            print("Liste ist leer")
+        else:
+            return self.list[index]
 
+    def sortAsc(self):
+        for i in range(1, len(self.list)):
             key = self.list[i]
             j = i - 1
             while j >= 0 and key < self.list[j]:
@@ -26,11 +49,14 @@ class ArrayList:
                 j -= 1
             self.list[j + 1] = key
 
-    def findByIndex(self, index):
-        if self.list is None:
-            print("Liste ist leer")
-        else:
-            return self.list[index]
+    def sortDesc(self):
+        for i in range(1, len(self.list)):
+            key = self.list[i]
+            j = i - 1
+            while j >= 0 and key > self.list[j]:
+                self.list[j + 1] = self.list[j]
+                j -= 1
+            self.list[j + 1] = key
 
 
 def Main():
@@ -40,17 +66,24 @@ def Main():
     if anzahl > 0:
         for i in range(0, anzahl):
             i = random.randint(0, anzahl)
-            a1.insertEnd(i)
-        print("\n")
+            a1.addEnd(i)
 
+    print("\n")
+    print("List without Insertion Sort: ")
+    print(a1.list)
+    print("\n")
+
+    print("List with Insertion Sort: ")
     start = time.time()
-    a1.insertionSort()
+    a1.sortAsc()
     end = time.time()
-
     print(a1.list)
 
     print("\n")
-    print("Zeit für Insertion Sort: {:5.3f}s".format(end - start))
+    # print("Length of the List: " a1.length() "!")
+
+    print("\n")
+    print("Zeit für Insertion Sort: {:5.5f}s".format(end - start))
 
 
 if __name__ == "__main__":
